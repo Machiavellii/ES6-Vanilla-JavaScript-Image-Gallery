4f0fef22a14ddd13a30dd8058288a48ea9317f66
Machiavellii <33911723+Machiavellii@users.noreply.github.com>
2018-04-12 19:12:10 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+let current = document.querySelector('#current');
+let imgs = document.querySelectorAll('.imgs img');
+let opacity = 0.4;
+
+//Set first image opacity
+imgs[0].style.opacity = opacity;
+
+imgs.forEach(img =>  img.addEventListener('click', imgClick));
+
+function imgClick(e){
+
+    //Reset opacity 
+    imgs.forEach(img => (img.style.opacity = 1));
+
+    // Change current image to src of clicked img
+    current.src = e.target.src;
+
+    //Add opacity
+    e.target.style.opacity = opacity;
+
+    //Add fade-in class
+    current.classList.add('fade-in');
+
+    //Remove fade-in class after animation
+    setTimeout(() => current.classList.remove('fade-in'), 500);  
+}
